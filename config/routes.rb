c8ae378fe5b3b9a41ae35d030844ee8162a45024
lin xu <helado_a@hotmail.com>
2024-06-20 22:25:15 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: 'restaurants#index'
+  resources :restaurants, only: %i[index show new creat] do
+    resources :review, only: %i[create]
+  end
 end
